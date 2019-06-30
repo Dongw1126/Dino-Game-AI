cbeb59521b1297c1e2890f80fdb0291c843eae73
Dongw1126 <topblade71@naver.com>
2019-06-30 13:55:43 +0000

    feat : init network

--- Deep Q Learning/src/ai.py
+++ Deep Q Learning/src/ai.py
@@ -3,17 +3,9 @@ import pygame as pg
 import random
 import copy
 import pickle
-from game import Dino
+#from game import Dino
 
 REPLAY_MEMORY = 50000
-
-def rand():
-    return random.uniform(-RAND_INIT, RAND_INIT)
-
-def np_rand(arr, row ,col):
-    for i in range(row):
-        for j in range(col):
-            arr[i][j] = rand()
 
 def ReLU(x):
     return np.maximum(0, x)
@@ -23,14 +15,29 @@ def Adam():
 
 class DQN:
     def __init__(self):
-        pass
+        self.input = np.array([1.0, 1.0])
+
+        self.init_network()
 
     def init_network(self):
-        pass
+        self.network = {}
+
+        W1 = np.random.rand((2, 3))
+        W2 = np.random.rand((3, 3))
+        W3 = np.random.rand((3, 3))
+
+        b1 = np.random.rand((1, 3))
+        b2 = np.random.rand((1, 3))
+        b3 = np.random.rand((1, 3))
+
+        self.network['W1'] = W1
+        self.network['W2'] = W2
+        self.network['W2'] = W2
+
+        self.network['b1'] = b1
+        self.network['b2'] = b2
+        self.network['b3'] = b3
 
     def predict(self):
         pass
-    
-            
-            
         
